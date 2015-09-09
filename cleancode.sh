6044ca7c590b9c48dd40afee8522b0982eb0932d
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2015-09-09 21:37:53 +0200

    Initial commit

---
+++ cleancode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Cleaning code in ${PWD} and subdirectories."
+# split output of find at newlines.
+IFS=$'\n'
+# send all relevant files to the code cleaner
+find | \
+    egrep "(\.cpp$)|(\.h$)|(\.in$)|(\.sh$)|(\.py$)|(\.txt$)|(.gitignore$)" | \
+    xargs ./tools/codecleaner.py
